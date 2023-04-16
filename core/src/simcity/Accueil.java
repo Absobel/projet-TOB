2d4cd415ac015aae08135069f607895940a1d8a4
nlermusi <nicolas.lermusiaux@etu.inp-toulouse.fr>
2023-04-16 13:30:19 +0200

    mtn soucis avec la premiere apparition de la fenetre

--- core/src/simcity/Accueil.java
+++ core/src/simcity/Accueil.java
@@ -4,8 +4,14 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.HexFormat;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -22,10 +28,16 @@ public class Accueil extends ScreenAdapter{
     private OrthographicCamera camera;
     private IsometricRenderer renderer;
     private Viewport viewport;
+    private Object quitterButton;
+    private Object jouerButton;
+
     
     public Accueil(SpriteBatch batch, Boolean dedans) {
         this.batch = batch;
         this.dansecranaccueil = dedans;
+
+
+
     }
     
     @Override
@@ -39,6 +51,8 @@ public class Accueil extends ScreenAdapter{
         renderer = new IsometricRenderer();
         menuTexture = new Texture(Gdx.files.internal("image-18-1024x585.png"));
         inputHandler = new InputHandler(camera, renderer.getGrid(), true);
+
+        
     }
     
     @Override
@@ -46,12 +60,16 @@ public class Accueil extends ScreenAdapter{
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
+
+
         batch.begin();
-        batch.draw(menuTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        int effectiveScreenWidth = (int) Math.floor(WIDTH*camera.zoom) ;
+        int effectiveScreenHeight = (int) Math.floor(HEIGHT*camera.zoom) ;
+        batch.draw(menuTexture, camera.position.x-effectiveScreenWidth/2, camera.position.y-effectiveScreenHeight/2, effectiveScreenWidth, effectiveScreenHeight);
         batch.end();
         inputHandler.handleInput(Gdx.graphics.getDeltaTime(), this.dansecranaccueil); // bool rajouté pour écran d'accueil
-        //this.dansecranaccueil = inputHandler.getBoolean();
-        //inputHandler.setBoolean(true);
+        //camera.update();
     }
     
     @Override
@@ -61,7 +79,9 @@ public class Accueil extends ScreenAdapter{
     }
     
     @Override
-    public void resize(int width, int height) {}
+    public void resize(int width, int height) {    // Called when the Application is resized.
+        viewport.update(width, height);
+    }
     
     @Override
     public void pause() {}
@@ -70,8 +90,8 @@ public class Accueil extends ScreenAdapter{
     public void resume() {}
     
     @Override
-    public void hide() { // pour pouvoir revenir si c'est demandé
-       // this.dansecranaccueil = false;
+    public void hide() {
+  
     }
 
     public boolean getEtat() { // recuperer dans les autres classes la valeure
