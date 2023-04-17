59d5a1524c32aac29f361c09681ab91be3a586f3
nlermusi <nicolas.lermusiaux@etu.inp-toulouse.fr>
2023-04-17 15:41:48 +0200

    boutons de menu

--- core/src/simcity/menuHUD.java
+++ core/src/simcity/menuHUD.java
@@ -1,72 +1,99 @@
 package simcity;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class menuHUD {
-    private Stage stage;
-    private SpriteBatch batch;
-    private Skin skin;
+public class menuHUD extends Stage{
+
+    private TextButton menu;
     private Table table;
-    private ButtonGroup<TextButton> buttonGroup;
-    
-    public menuHUD(SpriteBatch batch, Stage stage) {
-        this.batch = batch;
-        this.stage = stage;
-        
-        // Chargement de la skin
-        skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+    private Label titleLabel;
+    private BitmapFont font;
+    private Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+    public menuHUD(Viewport viewport, SpriteBatch batch) {
+        Gdx.input.setInputProcessor(this);  // Active la scène pour récupérer les événements d'entrée
 
         // Création de la table qui contiendra les boutons
         table = new Table();
-        table.setFillParent(true);  // Remplir tout l'écran
-        stage.addActor(table);
+        table.setFillParent(true);
+        table.right();
+        this.addActor(table);
 
-        // Création des boutons
-        TextButton button1 = new TextButton("Menu",skin);
-        TextButton button2 = new TextButton("Construction",skin);
+        // Ajout du titre
+        menu = new TextButton("Menu", skin);
+        menu.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                show();
+            }
+        });
 
         
-        //button1.add(new Image(new Texture("button1.png")));  // Ajout de l'image pour chaque bouton
-        //button2.add(new Image(new Texture("button2.png")));
-        //button3.add(new Image(new Texture("button3.png")));
-        button1.setPosition(700, 200);
-        button2.setPosition(700, 400);
 
+        // Ajout des boutons
+        TextButton resumeButton = new TextButton("Resume", skin);
+        TextButton restartButton = new TextButton("Restart", skin);
+        TextButton quitButton = new TextButton("Quit", skin);
 
-        stage.addActor(button1);
-        // Création du groupe de boutons pour n'avoir qu'un seul bouton sélectionné à la fois
-        buttonGroup = new ButtonGroup<>(button1, button2);
-        buttonGroup.setMaxCheckCount(1);   // On ne peut sélectionner qu'un bouton à la fois
-        buttonGroup.setMinCheckCount(0);   // On peut désélectionner les boutons
+        // Ajout d'un listener pour chaque bouton
+        resumeButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                hide();
+            }
+        });
+
+        restartButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Code pour redémarrer le jeu
+            }
+        });
+
+        quitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Code pour quitter le jeu
+            }
+        });
 
         // Ajout des boutons à la table
-        table.add(button1);
-        table.add(button2);
+        table.add(menu).padTop(50f).row();
+        table.add(resumeButton).padTop(50f).row();
+        table.add(restartButton).padTop(20f).row();
+        table.add(quitButton).padTop(20f).row();
+    }
 
-        // Ajout d'un listener pour chaque bouton pour récupérer le bouton sélectionné
-        button1.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                System.out.println("Bouton 1 sélectionné");
-            }
-        });
-        button2.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                System.out.println("Bouton 2 sélectionné");
-            }
-        });
+    public void show() {
+        Gdx.input.setInputProcessor(this);  // Active la scène pour récupérer les événements d'entrée
+    }
+
+    public void hide() {
+        Gdx.input.setInputProcessor(null);  // Désactive la scène pour ne plus récupérer les événements d'entrée
+    }
+
+    public void render() {
+        this.act();
+        this.draw();
+    }
+
+    public void dispose() {
+        this.dispose();
     }
 }
