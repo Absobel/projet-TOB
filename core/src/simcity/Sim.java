6ddb5587ba1edb65657c0af81ef559e1bdab820b
bubblegumcherrys <126185460+bubblegumcherrys@users.noreply.github.com>
2023-04-17 18:15:32 +0200

    Update Sim.java

--- core/src/simcity/Sim.java
+++ core/src/simcity/Sim.java
@@ -1,5 +1,27 @@
 package simcity;
-
+/*Un sim est un habitant de simcity
+ * il a  un batiment ou il habite et un numero de bonheur qui indique sa satisfaction
+ */
 classe Sim {
+
+	private Batiment building;
+	private int happiness; 
+	
+	public Sim(Batiment buil) {
+		this.building= buil;
+		this.happiness = 100;
+	}
+	
+	public void updateHapiness(boolean est_satisfait) {
+		if(est_satisfait) {
+			if(this.happiness<100) {
+				this.happiness = this.happiness + 1;
+			}
+		} else {
+			if(0<this.happines) {
+				this.happiness = this.happiness - 1;
+			}
+		}
+	}
   
 }
