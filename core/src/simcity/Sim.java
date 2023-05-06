df1286dc2d30ca024fb313e61eb506af98e3b64a
VJansou <vincent.jansou18@gmail.com>
2023-05-06 17:20:46 +0200

    petite correction

--- core/src/simcity/Sim.java
+++ core/src/simcity/Sim.java
@@ -2,7 +2,7 @@ package simcity;
 /*Un sim est un habitant de simcity
  * il a  un batiment ou il habite et un numero de bonheur qui indique sa satisfaction
  */
-classe Sim {
+public class Sim {
 
 	private Batiment building;
 	private int happiness; 
@@ -18,7 +18,7 @@ classe Sim {
 				this.happiness = this.happiness + 1;
 			}
 		} else {
-			if(0<this.happines) {
+			if(0<this.happiness) {
 				this.happiness = this.happiness - 1;
 			}
 		}
