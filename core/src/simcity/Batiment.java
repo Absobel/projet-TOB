7cfbbe644ff5ebbd772cb55f88c48b39444b0a23
Absobel <imkonnu@gmail.com>
2023-05-15 17:20:01 +0200

    batiment commentary pour compile

--- core/src/simcity/Batiment.java
+++ core/src/simcity/Batiment.java
@@ -1,9 +1,10 @@
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
+package simcity;
+
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
-
-package simcity;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -26,9 +27,9 @@ public class Batiment extends Tile { //batiment le plus classique va faire herit
         this.prix = prix;
     }
 
-    public void saveToJson(String filePath) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File(filePath);
-        objectMapper.writeValue(file, this);
-    }
+    // public void saveToJson(String filePath) throws IOException {
+    //     ObjectMapper objectMapper = new ObjectMapper();
+    //     File file = new File(filePath);
+    //     objectMapper.writeValue(file, this);
+    // }
 }
